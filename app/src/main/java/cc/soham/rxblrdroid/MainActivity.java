bda2b2b7e940094058416f75b62cca2b4ca825f6
Soham Mondal <soham@triveous.com>
2015-10-17 20:12:27 +0530

    Added retrolambda support, also added support for combining and sorting two network responses

--- app/src/main/java/cc/soham/rxblrdroid/MainActivity.java
+++ app/src/main/java/cc/soham/rxblrdroid/MainActivity.java
@@ -1,19 +1,22 @@
 package cc.soham.rxblrdroid;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cc.soham.rxblrdroid.network.Music;
+import cc.soham.rxblrdroid.network.MusicAdapter;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.recyclerview)
@@ -35,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.fab)
     public void fabOnClick(View view) {
         Snackbar.make(view, "Loading", Snackbar.LENGTH_LONG).show();
+        Music.getSortedZippedMusicList("Metallica", "Iron Maiden")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(musicResults -> recyclerView.setAdapter(new MusicAdapter(musicResults)));
     }
 
     @Override
